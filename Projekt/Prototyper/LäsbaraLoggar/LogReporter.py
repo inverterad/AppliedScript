4a4ca91882412364c816f8d8e9871d66e4edf530
F <xskoll@gmail.com>
2025-12-12 14:23:26 +0100

    Läsbara Loggar kommer nog bli projektet. Lagt till en del.

--- Projekt/Prototyper/LäsbaraLoggar/LogReporter.py
+++ Projekt/Prototyper/LäsbaraLoggar/LogReporter.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 #“Human-Readable Security Reports” from Raw Data
 # Many logs are unreadable.
 # Your script could take messy input (e.g., firewall logs, SSH logs) and convert it into:
@@ -8,8 +10,46 @@
 
 import subprocess
 
-# Läs in loggen
+# Läs in SSH-logg med hjälp av biblioteket subprocess, text=True levererar all data i en lång sträng.
 
-raw_ssh_data = subprocess.check_output(["journalctl", "-u", "ssh.service", "--no-pager", "--boot"], text=True)
+raw_ssh_data = subprocess.check_output(["journalctl", "-u", "ssh.service", "--no-pager"], text=True)
 
-print(raw_ssh_data)
+# ssh_success_count = 0
+# ssh_failure_count = 0
+ssh_examples = []
+
+# Loop för att gå genom varje logg-inlägg, eftersom det är sparat som en enda strängen så delar vi upp strängen här också.
+
+for line in raw_ssh_data.splitlines():
+    if "Accepted password" in line:
+        # ssh_success_count += 1
+        if len(ssh_examples) < 5:
+            ssh_examples.append(line.strip())
+    elif "Failed password" in line:
+        # ssh_failure_count += 1
+        if len(ssh_examples) < 5:
+            ssh_examples.append(line.strip())
+
+if len(ssh_examples) == 0:
+    print("No SSH attempts since yesterday.")
+else:
+    for line in ssh_examples:
+        print(line)
+
+###
+
+raw_sudo_data = subprocess.check_output(["journalctl", "-t", "sudo", "--since", "yesterday", "--no-pager"], text=True)
+
+raw_login_data = subprocess.check_output(["journalctl", "_SYSTEMD_UNIT=systemd-logind.service", "--since", "yesterday", "--no-pager"], text=True)
+
+raw_su_data = subprocess.check_output(["journalctl", "_COMM=su", "--since", "yesterday", "--no-pager"], text=True)
+
+
+
+
+# print(ssh_examples)
+# print(ssh_success_count)
+# print(ssh_failure_count)
+# print(raw_sudo_data)
+# print(raw_login_data)
+# print(raw_su_data)
