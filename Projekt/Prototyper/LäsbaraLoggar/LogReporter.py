b6524bc99b174ead2e6dcb7e4c8c3cf1258ecc77
illaluktande <xskoll@gmail.com>
2025-12-12 09:54:28 +0100

    En ny prototyp, får se om det blir något.

---
+++ Projekt/Prototyper/LäsbaraLoggar/LogReporter.py
@@ -0,0 +1,15 @@
+#“Human-Readable Security Reports” from Raw Data
+# Many logs are unreadable.
+# Your script could take messy input (e.g., firewall logs, SSH logs) and convert it into:
+# A simple table
+# A daily HTML summary
+# A CSV timeline
+# Real use: SOC analysts and admins depend on small conversion utilities daily.
+
+import subprocess
+
+# Läs in loggen
+
+raw_ssh_data = subprocess.check_output(["journalctl", "-u", "ssh.service"], text=True)
+
+print(raw_ssh_data)
