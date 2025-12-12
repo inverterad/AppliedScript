479bc9b9d2764b3a633b3581a15bcc5ed66ff29e
F <xskoll@gmail.com>
2025-12-12 03:55:58 -0500

    Vet inte riktigt vad jag ändrat, men något var det.

--- Projekt/Prototyper/SystemChecker/system-checker.sh
+++ Projekt/Prototyper/SystemChecker/system-checker.sh
@@ -2,6 +2,16 @@
 
 # Intro
 
-actual_version=$( cat /etc/os-release | grep "VERSION" | sed "s/[0-9]//g")
+# actual_version=$( cat /etc/os-release | grep "VERSION" | head -n 2 | tail -n 1 | sed 's/[^0-9.]//g' )
 
-echo $actual_version
+latest_os_upgrade=$( grep "upgrade linux-image" /var/log/dpkg.log | tail -n 1 | sed "s/ .*//" )
+current_os=$( uname -a )
+
+echo "-- System Status --"
+echo "Your system was last updated on $latest_os_upgrade."
+echo "Current OS : $current_os."
+echo
+echo "-- sudo users --"
+getent group sudo
+
+# EOF
